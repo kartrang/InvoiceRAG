0a1e380e0e2197d0298094997cdcba188d7dc36a
kartrang <karthickrangarajalu@gmail.com>
2026-01-27 00:17:47 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull kartrang/devopsfullcycle-app:latest
+docker pull kartrang/ragatmos-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 8501:8501 kartrang/devopsfullcycle-app 
+docker run -d -p 8501:8501 kartrang/ragatmos-app
