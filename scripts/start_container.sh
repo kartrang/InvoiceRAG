f97aa4ceb397fb3f286342595293ff857c2cc711
kartrang <karthickrangarajalu@gmail.com>
2026-01-26 13:46:43 +0530

    Fix docker pull command by removing unnecessary dot

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull kartrang/devopsfullcycle-app:latest .
+docker pull kartrang/devopsfullcycle-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 8501:8501 kartrang/devopsfullcycle-app 
