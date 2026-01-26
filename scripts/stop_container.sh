b27bc8c24e28cdc5a28d2f3ddecd52c5b80a852f
kartrang <karthickrangarajalu@gmail.com>
2026-01-26 14:05:20 +0530

    Update stop_container.sh to simplify container handling
    Modified script to stop running containers with a simplified message.

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -e
 
-# Stop and remove all running containers (if any)
-containerids=$(docker ps -q)
-
-if [ -n "$containerids" ]; then
-  echo "Stopping and removing running containers: $containerids"
-  docker rm -f $containerids
-else
-  echo "No running containers to stop."
-fi
+# Stop the running container (if any)
+echo "Hi"
